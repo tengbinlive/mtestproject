459381116fd8b60ceec063442ce41eda66dc4a88
tengbin <tengbinlive@gmail.com>
2016-06-03 18:32:41 +0800

    no message

--- app/src/main/java/com/binteng/activity/StickerActivity.java
+++ app/src/main/java/com/binteng/activity/StickerActivity.java
@@ -1,9 +1,21 @@
 package com.binteng.activity;
 
+import android.graphics.RectF;
+import android.widget.ImageView;
+
 import com.binteng.AbsActivity;
 import com.binteng.R;
+import com.binteng.mview.StickerView;
+
+import butterknife.BindView;
 
 public class StickerActivity extends AbsActivity {
+
+
+    @BindView(R.id.sticker_view)
+    StickerView stickerView;
+    @BindView(R.id.active_rect)
+    ImageView activeRect;
 
     @Override
     public int getLayoutID() {
@@ -14,4 +26,16 @@ public class StickerActivity extends AbsActivity {
     public void initData() {
         super.initData();
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        // 这里来获取容器的宽和高
+        if (hasFocus) {
+            RectF rectF = new RectF();
+            rectF.set(activeRect.getLeft(), activeRect.getTop(), activeRect.getRight(), activeRect.getBottom());
+            stickerView.setmWidgetRect(rectF);
+        }
+    }
+
 }
--- app/src/main/java/com/binteng/mview/StickerView.java
+++ app/src/main/java/com/binteng/mview/StickerView.java
@@ -12,32 +12,48 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.binteng.R;
+import com.nineoldandroids.animation.ObjectAnimator;
 import com.nineoldandroids.view.ViewHelper;
 
 public class StickerView extends RelativeLayout {
 
-    private int MAX_OVER_RESISTANCE = 0;
+    private final static int DURATION_ROTATION_TIME = 300;
+    private final static float MAX_SCALE = 3.0f;
 
     private ImageView img;
     private ImageView delete;
     private ImageView flip;
     private ImageView scale;
 
+    private float mMaxScale;
+
+    private boolean isOverturn;
+
     private boolean isEnable = true;
 
-    private boolean isAnimationRuning;
+    private boolean isOperating;
 
-    private boolean isInit;
-
-    private GestureDetector mDetector;
-
+    //活动区域
     private RectF mWidgetRect = new RectF();
-    private RectF mImgRect = new RectF();
 
     private View rootView;
 
-    private int mTranslateX;
-    private int mTranslateY;
+    private float lastX, lastY;
+
+    private float prevDegrees;
+
+    private GestureDetector mDetector;
+    private GestureDetector mRotateOrScaleDetector;
+
+    private OnStickerListener onStickerListener;
+
+    public interface OnStickerListener {
+        void onDelete();
+
+        void onFlip();
+
+        void onScale();
+    }
 
     public StickerView(Context context) {
         super(context);
@@ -52,12 +68,14 @@ public class StickerView extends RelativeLayout {
     private void init(Context context) {
         rootView = LayoutInflater.from(context).inflate(R.layout.layout_sticker_view, this, true);
         mDetector = new GestureDetector(getContext(), mGestureListener);
-
+        mRotateOrScaleDetector = new GestureDetector(getContext(), mScaleListener);
         img = (ImageView) getRootView().findViewById(R.id.img);
         delete = (ImageView) getRootView().findViewById(R.id.delete);
         flip = (ImageView) getRootView().findViewById(R.id.flip);
         scale = (ImageView) getRootView().findViewById(R.id.scale);
 
+
+        //默认活动区域 屏幕内
         WindowManager wm = (WindowManager) getContext()
                 .getSystemService(Context.WINDOW_SERVICE);
 
@@ -65,12 +83,65 @@ public class StickerView extends RelativeLayout {
         int height = wm.getDefaultDisplay().getHeight();
         mWidgetRect.set(0, 0, width, height);
 
-        float density = getResources().getDisplayMetrics().density;
-        MAX_OVER_RESISTANCE = (int) (density * 140);
+        mMaxScale = MAX_SCALE;
 
-        mTranslateX = 0;
-        mTranslateY = 0;
+        initListener();
+    }
 
+    private void initListener() {
+        delete.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != onStickerListener) {
+                    onStickerListener.onDelete();
+                }
+            }
+        });
+        flip.getParent().requestDisallowInterceptTouchEvent(true);
+        flip.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggerImage();
+                if (null != onStickerListener) {
+                    onStickerListener.onFlip();
+                }
+            }
+        });
+        scale.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (isEnable) {
+                    mRotateOrScaleDetector.onTouchEvent(event);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        img.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (isEnable) {
+                    mDetector.onTouchEvent(event);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void toggerImage() {
+        rotationView(img, "rotationY", isOverturn ? 180 : 0, isOverturn ? 0 : 180);
+    }
+
+    private void rotationView(View view, String type, float fromValues, float toValues) {
+        isOverturn = !isOverturn;
+        ObjectAnimator.ofFloat(view, type, fromValues, toValues).setDuration(DURATION_ROTATION_TIME).start();
+
+    }
+
+    public void setOnStickerListener(OnStickerListener onStickerListener) {
+        this.onStickerListener = onStickerListener;
     }
 
     public RectF getmWidgetRect() {
@@ -89,86 +160,127 @@ public class StickerView extends RelativeLayout {
         isEnable = enable;
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        if (!isInit) {
-            isInit = true;
-            mImgRect.set(getLeft(), getTop(), getRight(), getBottom());
-        }
+    public float getmMaxScale() {
+        return mMaxScale;
     }
 
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent event) {
-        if (isEnable) {
-            mDetector.onTouchEvent(event);
-            return true;
-        } else {
-            return super.dispatchTouchEvent(event);
-        }
+    public void setmMaxScale(float mMaxScale) {
+        this.mMaxScale = mMaxScale;
+    }
+
+    /**
+     * 不能移出活动区域
+     *
+     * @param nextX
+     * @return
+     */
+    private float calculateActiveX(float nextX) {
+        if (nextX < mWidgetRect.left)
+            nextX = mWidgetRect.left;
+        else if (nextX > mWidgetRect.right - getWidth())
+            nextX = mWidgetRect.right - getWidth();
+        return nextX;
+    }
+
+    /**
+     * 不能移出活动区域
+     *
+     * @param nextY
+     * @return
+     */
+    private float calculateActiveY(float nextY) {
+        if (nextY < mWidgetRect.top)
+            nextY = mWidgetRect.top;
+        else if (nextY > mWidgetRect.bottom - getWidth())
+            nextY = mWidgetRect.bottom - getWidth();
+        return nextY;
     }
 
     private GestureDetector.OnGestureListener mGestureListener = new GestureDetector.SimpleOnGestureListener() {
 
+        @Override
+        public boolean onDown(MotionEvent event) {
+            lastX = event.getRawX();
+            lastY = event.getRawY();
+            return false;
+        }
 
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            if (isAnimationRuning) {
-
-            }
-
-            if (canScrollHorizontallySelf(distanceX)) {
-                if (distanceX < 0 && mImgRect.left - distanceX > mWidgetRect.left)
-                    distanceX = mImgRect.left;
-                if (distanceX > 0 && mImgRect.right - distanceX < mWidgetRect.right)
-                    distanceX = mImgRect.right - mWidgetRect.right;
-
-                mTranslateX -= distanceX;
-            }
-
-            if (canScrollVerticallySelf(distanceY)) {
-                if (distanceY < 0 && mImgRect.top - distanceY > mWidgetRect.top)
-                    distanceY = mImgRect.top;
-                if (distanceY > 0 && mImgRect.bottom - distanceY < mWidgetRect.bottom)
-                    distanceY = mImgRect.bottom - mWidgetRect.bottom;
-
-                mTranslateY -= distanceY;
-            }
-
-            float left = getLeft();
-            float right = getRight();
-//            ViewHelper.setTranslationX(rootView, mTranslateX);
-//            ViewHelper.setTranslationY(rootView, mTranslateY);
+            moveView(e2);
             return true;
         }
 
     };
 
+    private void moveView(MotionEvent event) {
+        float distanceX = lastX - event.getRawX();
+        float distanceY = lastY - event.getRawY();
 
-    private float resistanceScrollByX(float overScroll, float detalX) {
-        return detalX * (Math.abs(Math.abs(overScroll) - MAX_OVER_RESISTANCE) / (float) MAX_OVER_RESISTANCE);
+        float nextY = calculateActiveY(getY() - distanceY);
+        float nextX = calculateActiveX(getX() - distanceX);
+
+        ViewHelper.setX(rootView, nextX);
+        ViewHelper.setY(rootView, nextY);
+
+        lastX = event.getRawX();
+        lastY = event.getRawY();
     }
 
-    private float resistanceScrollByY(float overScroll, float detalY) {
-        return detalY * (Math.abs(Math.abs(overScroll) - MAX_OVER_RESISTANCE) / (float) MAX_OVER_RESISTANCE);
+    private GestureDetector.OnGestureListener mScaleListener = new GestureDetector.SimpleOnGestureListener() {
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            lastX = event.getRawX();
+            lastY = event.getRawY();
+            return false;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            rotateOrScale(e2);
+            return true;
+        }
+
+    };
+
+    private void rotateOrScale(MotionEvent event) {
+        rotateView(event);
+//        scaleView();
+        lastX = event.getRawX();
+        lastY = event.getRawY();
+
     }
 
-    public boolean canScrollHorizontallySelf(float direction) {
-        if (mImgRect.width() <= mWidgetRect.width()) return false;
-        if (direction < 0 && Math.round(mImgRect.left) - direction >= mWidgetRect.left)
-            return false;
-        if (direction > 0 && Math.round(mImgRect.right) - direction <= mWidgetRect.right)
-            return false;
-        return true;
+    private void rotateView(MotionEvent event) {
+
+        float distanceX = event.getRawX() - lastX;
+        float distanceY = event.getRawY() - lastY;
+
+        float k = distanceY / distanceX;   //斜率
+
+        float currDegrees = (float) Math.toDegrees(Math.atan(k));
+
+        prevDegrees += currDegrees;
+        ViewHelper.setRotation(rootView, prevDegrees);
     }
 
-    public boolean canScrollVerticallySelf(float direction) {
-        if (mImgRect.height() <= mWidgetRect.height()) return false;
-        if (direction < 0 && Math.round(mImgRect.top) - direction >= mWidgetRect.top)
-            return false;
-        if (direction > 0 && Math.round(mImgRect.bottom) - direction <= mWidgetRect.bottom)
-            return false;
-        return true;
+    private void scaleView(float scale) {
+        if (scale < 1) {
+            scale = 1;
+        } else if (scale > mMaxScale) {
+            scale = mMaxScale;
+        }
+        ViewHelper.setScaleX(rootView, scale);
+        ViewHelper.setScaleX(rootView, scale);
+    }
+
+    private float caculateSlope(MotionEvent event) {
+        float x1 = lastX;
+        float y1 = lastY;
+        float x2 = event.getRawX();
+        float y2 = event.getRawY();
+        return (y2 - y1) / (x2 - x1);
     }
 
 }
